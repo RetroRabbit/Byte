abea1d6865aeaa1930cbc669a843082da30e7490
deanvd <wilkendean@gmail.com>
2018-01-09 14:32:54 +0200

    Login page corrected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import HeaderComponent from './components/HeaderComponent';
+import SideComponent from './components/SideComponent';
+import ChatComponent from './components/ChatComponent';
+import LoginComponent from './components/LoginComponent';
+
 import logo from './Images/logo.svg';
-import './Css/App.css';
-import LoginComponent from "./components/LoginComponent";
-import ChatComponent from "./components/ChatComponent";
-import HeaderComponent from "./components/HeaderComponent";
-import SideComponent from "./components/SideComponent";
 
-class App extends Component {
-  render() {
-      return <div className="App">
+class App extends Component{
+render(){
+ return(
+<div className="App">
+<MuiThemeProvider>
 <LoginComponent/>
-
-
-
-
-      </div>;
-
-  }
-
-}
+  </MuiThemeProvider>
+  </div>
+ );
+}}
 
 export default App;
