a3a29f152e2ebcc6c28bb09b89567e56eade7908
Martinthebunny <mmese@retrorabbit.co.za>
2018-01-09 11:49:50 +0200

    Approved Pull Request

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
     <BrowserRouter>
         <div>
             <Route exact path="/" component={App}>Login</Route>
-            <Route path="/settings" component={Settings}></Route>
+            <Route path="/Settings" component={Settings}></Route>
         </div>
     </BrowserRouter>
 
