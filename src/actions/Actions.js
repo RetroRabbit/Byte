10a03f4d8deb2d41e53a388c0e59bf54b56861d9
RashadAkoodieRabbit <rakoodie@retrorabbit.co.za>
2018-01-10 09:41:14 +0200

    Added action.js file

---
+++ src/actions/Actions.js
@@ -0,0 +1,23 @@
+export const LoginToReg = () => {
+    return{
+        type: 'GO_TO_REG'
+    }
+}
+
+export const RegToSecondReg = () => {
+    return{
+        type: 'GO_TO__SECOND_REG'
+    }
+}
+
+export const RegToThirdReg = () => {
+    return{
+        type: 'GO_TO_THIRD_REG'
+    }
+}
+
+export const LoginToHome = () => {
+    return{
+        type: 'GO_TO_HOME'
+    }
+}
