eae701481c64b8b8c68d9cde474196e0e8bba895
RashadAkoodieRabbit <rakoodie@retrorabbit.co.za>
2018-01-10 08:52:53 +0200

    Starte3d to import Redux store into the project

--- src/index.js
+++ src/index.js
@@ -29,19 +29,4 @@ ReactDOM.render(
     , document.getElementById('root'));
 registerServiceWorker();
 
-ReactDOM.render(
-    
-    <BrowserRouter>
-        <div>
-            <Route exact path="/" component={Login}>Login</Route>
-            <Route path="/settings" component={Settings}></Route>
-            <Route path="/registration" component={Registration}></Route>
-            <Route path="/stepone" component={Registerstepone}></Route>
-            <Route path="/steptwo" component={Registersteptwo}></Route>
-            <Route path="/stepthree" component={Registerstepthree}></Route>
-            <Route path="/mainChat" component={MainChat}></Route>
-        </div>
-    </BrowserRouter>
 
-    , document.getElementById('root'));
-registerServiceWorker();
